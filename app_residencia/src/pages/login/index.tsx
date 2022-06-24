86623797a25ce94003aa790a3272356400358834
Talles Dias <tallesdelfino1@gmail.com>
2022-06-23 22:07:06 -0300

    Segundo dia de aula

---
+++ app_residencia/src/pages/login/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Button, Icon, Input, Text } from "react-native-elements";
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const handleLogin = ({ email, senha }) => {
+        console.log("Email : ", email, "Senha :", senha);
+
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.texto_entrada}>{'Bem-vindo'}</Text>
+            <Input
+                placeholder='E-mail'
+                onChangeText={setEmail}
+                value={email}
+                leftIcon={<Icon name='user' color='#000' type='font-awesome' size={24} />}
+            />
+            <Input
+                placeholder='E-mail'
+                onChangeText={setSenha}
+                value={senha}
+                leftIcon={<Icon name='key' color='#000' type='font-awesome' size={24} />}
+            />
+
+            <Button
+                title='Entrar'
+                onPress={() => handleLogin({ email, senha })}
+                titleStyle={styles.title_Button}
+            />
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7054B6',
+        padding: 16,
+        alignItems: 'stretch',
+        justifyContent: 'center'
+
+    },
+    texto_entrada: {
+        fontWeight: 'bold',
+        fontSize: 30,
+        marginBottom: 10,
+        color: '#ffffff',
+
+    },
+    title_Button: {
+        fontSize: 30,
+    }
+})
+
+export default Login;
