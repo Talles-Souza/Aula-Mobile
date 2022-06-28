75d13ffcf4a32ca00e25e671e2e3f3b7b4fbe4d1
Talles Dias <tallesdelfino1@gmail.com>
2022-06-27 21:19:58 -0300

    mais alguma coisa

--- app_residencia/src/pages/home/index.tsx
+++ app_residencia/src/pages/home/index.tsx
@@ -6,11 +6,36 @@ import { MyCardImg2 } from "../../components/card/cardimg2";
 import { View } from "react-native";
 import { Title } from "react-native-paper";
 import { ScrollView } from "react-native-gesture-handler";
+import Axios from "../../api/axios";
+import { useEffect, useState } from "react";
+
+type CategoriaType = {
+    idCategoria: number;
+    nomeCategoria: string;
+    nomeImagem: string;
+}
 
 
 const Home = ({ route, navigation }) => {
 
     const { token } = route.params;
+    const [categoria, setCategoria] = useState<CategoriaType[]>([]);
+
+useEffect(() =>{
+
+}, []);
+
+    const getDadosCategoria = async () => {
+        Axios.get(
+            '/categoria',
+            { headers: { "Authorization": `Bearer ${token}` } }
+
+        ).then(result => {
+            console.log("dados das categorias" + JSON.stringify(result.data));
+            setCategoria(result.data)
+        });
+    }
+
     console.log('Token' + token);
 
     return (
