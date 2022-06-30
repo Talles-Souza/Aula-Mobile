54bef1d27895136d4df70f63c2d6b923abc43974
Talles Dias <tallesdelfino1@gmail.com>
2022-06-30 15:18:22 -0300

    falta navegação para categoria

--- app_residencia/src/pages/categorias/index.tsx
+++ app_residencia/src/pages/categorias/index.tsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ScrollView } from "react-native-gesture-handler";
-import { Text } from "react-native-elements";
+import Axios from "../../api/axios";
 import MySearch from '../../components/search';
 import { StyleSheet } from "react-native";
+import { AutenticacaoContext } from "../../context/AutenticacaoContext";
+import MyCard from "../../components/card/card";
+import { View } from "react-native-ui-lib";
+import { FlatList } from "react-native";
+import Card2 from "../../components/card/card2";
 
 
 
 const Categorias = () => {
+
+    //get categoria ainda nao está pronto
+
+    const { usuario } = useContext(AutenticacaoContext);
+    // const { token } = route.params;
+    const [categorias, setCategoria] = useState<any[]>([]);
+
+    useEffect(() => {
+        getDadosCategoria();
+    }, []);
+
+    const getDadosCategoria = async () => {
+        Axios.get(
+            '/categoria',
+            { headers: { "Authorization": `Bearer ${usuario.token}` } }
+
+        ).then(result => {
+            console.log("dados das categorias" + JSON.stringify(result.data));
+            setCategoria(result.data)
+        }).catch((error) => {
+            console.log("Erro ao carregar " + JSON.stringify(error));
+
+        });
+    }
+
+
+
     return (
         <ScrollView style={styles.container}>
             <MySearch />
+            <Card2 />
+
+
+
+            {/* 
+            <View style={styles.view}
+            
+            >
+                {categorias.map((categoria, indice) => (
+                    <MyCard
+                        key={indice}
+                        dados={categoria}
+                    />))}
+            </View> */}
+
+
         </ScrollView>
     );
 }
@@ -17,6 +65,15 @@ const Categorias = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+
+    },
+    view: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+
 
     }
 })
